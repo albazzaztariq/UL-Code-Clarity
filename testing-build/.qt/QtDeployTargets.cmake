cd11b54e16925bc6d2c4af24a96497959b7a1871
Tariq Albazzaz <albazzaztariq@users.noreply.github.com>
2026-03-21 08:43:03 -0600

    Remove testing-build from repo, add to gitignore

--- testing-build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_CodeClarity_FILE C:/Users/azt12/OneDrive/Documents/Computing/LANG/UniLogic/Code Clarity/src/build/CodeClarity.exe)
-set(__QT_DEPLOY_TARGET_CodeClarity_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_CodeClarity_RUNTIME_DLLS C:/Qt/6.8.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Core.dll)
